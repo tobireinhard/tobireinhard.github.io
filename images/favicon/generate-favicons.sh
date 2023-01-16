9d8c936edccb9d5a83b347848e15d21867a1081e
Tobias Reinhard <16916681+tobireinhard@users.noreply.github.com>
2023-01-16 11:13:31 +0100

    Added script to generate favicons from SVG file.

---
+++ images/favicon/generate-favicons.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This script generates favicons in various dimensions from an SVG file.
+# It must be run from the directory containing the script.
+# The created favicons are stored in the directory containing the script.
+#
+# Expected arguments:
+# $1 : Path to SVG file from which the favicons shall be created.
+
+
+
+SVG_FILE=$1
+
+check_svg_file_arg()
+{
+  if [[ "$SVG_FILE" == "" ]] || [[ "$SVG_FILE" != *.svg ]]; then
+    echo "Expected svg file as argument, found:"
+    echo "\"$SVG_FILE\""
+    exit 1
+  fi
+}
+
+
+
+sizes=(
+  310x310
+  150x150
+  144x144
+  96x96
+  70x70
+  32x32
+  16x16
+)
+
+check_svg_file_arg "$SVG_FILE"
+echo Converting SVG file "$SVG_FILE" to PNGs
+
+for s in "${sizes[@]}"; do
+  echo Converting to size $s
+  out_file="favicon-$s.png"
+  convert -resize $s "$SVG_FILE" "$out_file"
+  echo "    -> $out_file"
+done
+
+echo Converting SVG file "$SVG_FILE" to ICO
+convert -resize 256x256 "$SVG_FILE" favicon.ico
+echo "    -> favicon.ico"
